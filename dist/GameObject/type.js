1f807fef7df4835dbe62fdb326dc1222f96d4c23
Ureimu <57444811+Ureimu@users.noreply.github.com>
2022-05-26 04:18:56 +0000

    Deploying to gh-pages from @ Ureimu/coll-binball@a926dcb84b7af2c23eaf92b7c0b75c374521ceed 🚀

---
+++ dist/GameObject/type.js
@@ -0,0 +1,12 @@
+import {NullCategory} from "./index.js";
+import {MarbleCategory} from "./Marble/index.js";
+import {SnagCategory} from "./Snag/index.js";
+import {SubSnagCategory} from "./Snag/subSnag.js";
+import {SnagWallCategory} from "./Snag/wall.js";
+export const Categories = {
+  NullCategory,
+  SnagCategory,
+  SnagWallCategory,
+  SubSnagCategory,
+  MarbleCategory
+};
--- dist/snagMap/testMap.js
+++ dist/snagMap/testMap.js
@@ -1,9 +1,9 @@
-import {snagNormalSnag} from "../GameObject/Snag/normalSnag.js";
+import {addSnagPair} from "../GameObject/Snag/index.js";
 export function testMap(scene, border) {
   const matter = scene.matter;
   const bricksList = [];
   for (let i = 0; i < 200; i++) {
-    bricksList.push(snagNormalSnag(scene, Phaser.Math.Between(border.left, border.right), Phaser.Math.Between(border.top, border.bottom)));
+    bricksList.push(addSnagPair("snag:normalSnag", scene, Phaser.Math.Between(border.left, border.right), Phaser.Math.Between(border.top, border.bottom)));
   }
-  return {initMarblePos: [{x: (border.left + border.right) / 2, y: border.top + 80}], snagList: bricksList};
+  return {initMarblePos: [{x: (border.left + border.right) / 2, y: border.top + 80}], snagPairList: bricksList};
 }
